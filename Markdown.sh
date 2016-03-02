9461a791a5e0bf375396bb855d6e109db0e33fe5
linxd5 <164532640@qq.com>
2016-03-02 20:11:42 +0800

    my blogs

---
+++ Markdown.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Date:  2015/12/14
+# Change1: delete the directory, so I can visit the files directly
+# Change2: delete the empty files
+
+export DISPLAY=:0.0    # 启动GUI显示
+
+haroopad /home/panjinbo/Desktop/git_blogs/README.md
+
---
+++ README.md
@@ -0,0 +1,6 @@
+因为CMD Markdown的内容无法同步到hexo博客上, 所以这篇博文主要用来记录每天在CMD Markdown上做的更改, 到了晚上再整理到博客上;
+
+### 2015年3月2号
+
+1. CMD Markdown + github + hexo
+2. 马尔科夫逻辑网络(1)
